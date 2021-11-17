5401a98baad081625d4ee7fd0c5da4c92ff0f382
Cem PEHLİVAN <cempehlivan92@gmail.com>
2021-11-17 23:34:18 +0300

    QNB Finansbank eklendi

---
+++ CP.VPOS/Banks/QNBFinansbank.cs
@@ -0,0 +1,15 @@
+﻿
+namespace CP.VPOS.Banks
+{
+    internal class QNBFinansbankVirtualPOSService : NestpayVirtualPOSService
+    {
+        private static readonly string _urlAPILive = "https://www.fbwebpos.com/fim/api";
+        private static readonly string _url3DLive = "https://www.fbwebpos.com/fim/est3Dgate";
+
+        //test panel: https://entegrasyon.asseco-see.com.tr/finans/report/user.login
+
+        public QNBFinansbankVirtualPOSService() : base(_urlAPILive, _url3DLive)
+        {
+        }
+    }
+}
